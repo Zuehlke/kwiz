9c0bf6fe04349cee8ed1bab8c2fa3301c8885639
Andreas Brüniger <andreas.bruniger@zuhlke.com>
2025-05-14 17:47:48 +0200

    init

---
+++ src/main/java/ch/zuhlke/camp/kwiz/KwizApplication.java
@@ -0,0 +1,13 @@
+package ch.zuhlke.camp.kwiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/zuhlke/camp/kwiz/KwizApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zuhlke.camp.kwiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
